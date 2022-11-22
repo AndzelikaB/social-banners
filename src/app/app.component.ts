4ecdff5c18fed50d93d2f64d4d86d9e612879015
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-11-22 15:31:05 +0100

    added ngStyle, ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'social-banners';
+  width = 600;
+  height = 500;
+  color1 = 'yellow';
+  color2 = 'pink';
+  percentColor1 = 0;
+  percentColor2 = 100;
+  fontSize = 52;
+
+  layout = {
+    horizontal: false,
+    horizontalRev: false,
+    vertical: false,
+    verticalRev: false,
+  };
+
+  colors = [
+    'red',
+    'purple',
+    'blue',
+    'green',
+    'yellow',
+    'brown',
+    'grey',
+    'black',
+  ];
+
+  getBannerStyles() {
+    return {
+      width: `${this.width}px`,
+      height: `${this.height}px`,
+      backgroundImage: `linear-gradient(to bottom, ${this.color1} ${this.percentColor1}%, ${this.color2} ${this.percentColor2}%)`,
+    };
+  }
 }
